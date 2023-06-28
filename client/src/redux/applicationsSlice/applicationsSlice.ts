894edf284cff73355860899ac3ae6b42f6a9f0c9
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-06-28 21:32:03 +0200

    Add more course options

--- client/src/redux/applicationsSlice/applicationsSlice.ts
+++ client/src/redux/applicationsSlice/applicationsSlice.ts
@@ -48,8 +48,13 @@ enum Device {
 }
 
 enum Course {
-  ITSE = 'Introduction to Software Engineering',
-  PISE = 'Patterns in Software Engineering',
+  ITSE = 'Introduction to Software Engineering (IN0006)',
+  PISE = 'Patterns in Software Engineering (IN2081)',
+  ITP = 'Introduction to Programming (CIT5230000)',
+  IPRAKTIKUM = 'iPraktikum (IN0012, IN2106, IN2128)',
+  JASS = 'Joint Advanced Student School',
+  FK = 'Ferienakademie',
+  THESIS = 'Thesis',
 }
 
 interface Student {
--- server/src/main/java/prompt/ls1/model/Course.java
+++ server/src/main/java/prompt/ls1/model/Course.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Course {
-    ITSE("Introduction to Software Engineering"), PISE("Pattern in Software Engineering");
+    ITSE("Introduction to Software Engineering (IN0006)"),
+    PISE("Patterns in Software Engineering (IN2081)"),
+    ITP("Introduction to Programming (CIT5230000)"),
+    IPRAKTIKUM("iPraktikum (IN0012, IN2106, IN2128)"),
+    JASS("Joint Advanced Student School"),
+    FK("Ferienakademie"),
+    THESIS("Thesis");
 
     private String value;
 }
