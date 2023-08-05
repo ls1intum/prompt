55a10d70ffb423b8fed8a0e8032f8f4ff9ff7a60
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-08-05 16:04:09 +0200

    Refactor mapping between application and intro course participation

---
+++ server/src/main/resources/db/changelog/changes/v0005.sql
@@ -0,0 +1,11 @@
+ALTER TABLE intro_course_participation
+    ADD COLUMN developer_application_id uuid,
+    ADD COLUMN tutor_application_id uuid;
+
+UPDATE intro_course_participation
+    SET developer_application_id = developer_application.id
+    FROM developer_application
+    WHERE intro_course_participation.id = developer_application.intro_course_participation_id;
+
+ALTER TABLE developer_application
+    DROP COLUMN intro_course_participation_id;
