240109c4a811ce5ebc21704bd8a8325d46efa59a
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-10-11 11:48:32 +0200

    Reorder nav items

--- client/src/utilities/NavigationBar/NavigationBar.tsx
+++ client/src/utilities/NavigationBar/NavigationBar.tsx
@@ -53,16 +53,16 @@ const navigationContents = [
     permission: [Permission.PM],
   },
   {
-    label: 'Team Allocation',
-    icon: IconUsers,
-    link: '/management/team-allocation',
-    permission: [Permission.PM],
-  },
-  {
     label: 'Intro Course',
     icon: IconSchool,
     link: '/management/intro-course',
     permission: [Permission.PM, Permission.TUTOR],
+  },
+  {
+    label: 'Team Allocation',
+    icon: IconUsers,
+    link: '/management/team-allocation',
+    permission: [Permission.PM],
   },
   {
     label: 'Infrastructure',
