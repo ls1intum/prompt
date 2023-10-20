070e1ed498a0cebb1d563550268a7220dd15a2cc
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-10-20 13:16:24 +0200

    Add notifications for accept/reject thesis success/error request

--- client/src/redux/thesisApplicationsSlice/thunks/acceptThesisApplication.ts
+++ client/src/redux/thesisApplicationsSlice/thunks/acceptThesisApplication.ts
@@ -1,24 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { serverBaseUrl } from '../../../service/configService'
+import { notifications } from '@mantine/notifications'
 
 export const acceptThesisApplication = createAsyncThunk(
   'thesisApplications/acceptThesisApplication',
 
   async (thesisApplicationId: string, { rejectWithValue }) => {
     try {
-      return (
-        await axios.post(
-          `${serverBaseUrl}/api/thesis-applications/${thesisApplicationId}/accept`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('jwt_token') ?? ''}`,
-            },
+      const response = await axios.post(
+        `${serverBaseUrl}/api/thesis-applications/${thesisApplicationId}/accept`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('jwt_token') ?? ''}`,
           },
-        )
-      ).data
+        },
+      )
+
+      if (response) {
+        notifications.show({
+          color: 'green',
+          autoClose: 5000,
+          title: 'Success',
+          message: `Sent an acceptance mail successfully.`,
+        })
+      }
+
+      return response.data
     } catch (err) {
+      notifications.show({
+        color: 'red',
+        autoClose: 5000,
+        title: 'Error',
+        message: `Failed to send an acceptance mail.`,
+      })
       return rejectWithValue(err)
     }
   },
--- client/src/redux/thesisApplicationsSlice/thunks/rejectThesisApplication.ts
+++ client/src/redux/thesisApplicationsSlice/thunks/rejectThesisApplication.ts
@@ -1,24 +1,40 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { serverBaseUrl } from '../../../service/configService'
+import { notifications } from '@mantine/notifications'
 
 export const rejectThesisApplication = createAsyncThunk(
   'thesisApplications/rejectThesisApplication',
 
   async (thesisApplicationId: string, { rejectWithValue }) => {
     try {
-      return (
-        await axios.post(
-          `${serverBaseUrl}/api/thesis-applications/${thesisApplicationId}/reject`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('jwt_token') ?? ''}`,
-            },
+      const response = await axios.post(
+        `${serverBaseUrl}/api/thesis-applications/${thesisApplicationId}/reject`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('jwt_token') ?? ''}`,
           },
-        )
-      ).data
+        },
+      )
+
+      if (response) {
+        notifications.show({
+          color: 'green',
+          autoClose: 5000,
+          title: 'Success',
+          message: `Sent a rejection mail successfully.`,
+        })
+      }
+
+      return response.data
     } catch (err) {
+      notifications.show({
+        color: 'red',
+        autoClose: 5000,
+        title: 'Error',
+        message: `Failed to send a rejection mail.`,
+      })
       return rejectWithValue(err)
     }
   },
