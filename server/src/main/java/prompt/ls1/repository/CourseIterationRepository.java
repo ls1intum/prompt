3717ec8e78b2878cb54edeeb6d4a60f2bf3eb466
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2024-02-06 14:40:54 +0100

    Try to fix save o course iteration update

--- server/src/main/java/prompt/ls1/repository/CourseIterationRepository.java
+++ server/src/main/java/prompt/ls1/repository/CourseIterationRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Transactional
 @Repository
 public interface CourseIterationRepository extends JpaRepository<CourseIteration, UUID> {
     @Transactional
