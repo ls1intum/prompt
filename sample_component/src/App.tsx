f3d0c09b70e20cb18136acfcc7327f188e4abcf7
Stefan Niclas Heun <niclas.heun@tum.de>
2024-10-15 14:41:11 +0200

    include eslint to sample component

--- sample_component/src/App.tsx
+++ sample_component/src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react"
+import React from "react";
 
 const App = (): JSX.Element => {
-  return (
-    <h1>
-      Sample Component
-    </h1>)
-}
-
+  return <h1>Sample Component</h1>;
+};
 
 export default App;
