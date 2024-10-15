bf5b1c4f8d7a06d3817f7e89d894155842869e3b
Stefan Niclas Heun <niclas.heun@tum.de>
2024-10-15 15:06:57 +0200

    doing some cleanup

--- sample_component/eslint.config.mjs
+++ sample_component/eslint.config.mjs
@@ -1,38 +1,38 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-});
+})
 
 export default [
   ...fixupConfigRules(
     compat.extends(
-      "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-      "plugin:prettier/recommended",
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:prettier/recommended',
     ),
   ),
   {
-    files: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"],
+    files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
 
     plugins: {
-      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
       react: fixupPluginRules(react),
-      "react-hooks": fixupPluginRules(reactHooks),
+      'react-hooks': fixupPluginRules(reactHooks),
     },
 
     languageOptions: {
@@ -42,34 +42,34 @@ export default [
       },
 
       parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
 
-        project: ["tsconfig.json"],
+        project: ['tsconfig.json'],
       },
     },
 
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
 
     rules: {
-      "no-shadow": "off",
-      "@typescript-eslint/no-shadow": ["error"],
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': ['error'],
 
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/strict-boolean-expressions": "off",
-      "@typescript-eslint/return-await": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/return-await': 'off',
 
-      "max-len": [
-        "warn",
+      'max-len': [
+        'warn',
         {
           code: 160,
           ignoreComments: true,
@@ -77,24 +77,24 @@ export default [
         },
       ],
 
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
 
-      "react/jsx-filename-extension": [
+      'react/jsx-filename-extension': [
         1,
         {
-          extensions: [".tsx", ".jsx"],
+          extensions: ['.tsx', '.jsx'],
         },
       ],
 
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
 
-      "prettier/prettier": [
-        "error",
+      'prettier/prettier': [
+        'error',
         {
-          endOfLine: "auto",
+          endOfLine: 'auto',
         },
       ],
     },
   },
-];
+]
